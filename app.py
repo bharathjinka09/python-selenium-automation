c4c9db57ffcf78687d40e7e02801339aac42aec3
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-05 12:28:29 +0530

    google search

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from selenium import webdriver
-import os
-from selenium.webdriver.chrome.options import Options
+import os,time
 
 driver = webdriver.Chrome(f"{os.getcwd()}/chromedriver")
+
 driver.get('https://www.google.com')
+time.sleep(2)
+
+search_input = driver.find_element_by_name('q')
+search_input.send_keys('Python Programming Language')
+
+time.sleep(2)
+
+search_btn = driver.find_element_by_css_selector("input[type='submit']")
+search_btn.click()
+
+time.sleep(2)
+
 driver.close()
